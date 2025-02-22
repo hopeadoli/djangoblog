a344a2cf93cdcc24275b6f2e22fce1aaaed3ed34
hopeadoli <adolihope@gmail.com>
2025-02-22 15:59:23 +0000

    add image save method

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 
 class Post(models.Model):
@@ -17,3 +21,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Save the model first
+
+        if self.featured_image:
+            img = Image.open(self.featured_image)
+
+            # Example: Resize the image
+            max_size = (800, 800)  # Adjust dimensions as needed
+            img.thumbnail(max_size)
+
+            # Save the processed image back to the ImageField
+            output = BytesIO()
+            img.save(output, format='JPEG', quality=85)  # Adjust format and quality
+
+            output.seek(0)
+            self.featured_image = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                f"{self.featured_image.name.split('.')[0]}.jpg",  # Use .jpg or other format
+                'image/jpeg',
+                sys.getsizeof(output),
+                None
+            )
+
+            super().save(*args, **kwargs) # Save the modified imagefield.
